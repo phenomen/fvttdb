523defd448b3b0303b405bf3c7b1c86a45003917
phenomen <phenomen@pm.me>
2024-10-13 09:29:50 -0500

    npm fix

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import path from "node:path";
 import { readdir } from "node:fs/promises";
 import { extractPack, compilePack } from "@foundryvtt/foundryvtt-cli";
